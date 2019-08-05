06cd6f3685dd7fafd121cb962da21a64f3b6a317
kennylam91 <phamvanlam1991@gmail.com>
2019-08-05 10:41:15 +0700

    create ITodo interface

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,10 +1,16 @@
 import {Component, OnInit} from '@angular/core';
 
+interface ITodo {
+  id: number;
+  content: string;
+  complete: boolean;
+}
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.scss']
 })
+
 export class TodoComponent {
   private todo = {
     id: 1,
